51d7e359a6ae8d281e3751be974cb8838eff04b5
Ahmet Emre Karaca <ahmetemre_k@hotmail.com>
2023-09-23 14:42:57 +0300

    swagger control has been removed

--- CosmosDbEntegrasyon.Api/Program.cs
+++ CosmosDbEntegrasyon.Api/Program.cs
@@ -16,12 +16,10 @@ builder.Services.AddMicrosoftCosmosDb(builder.Configuration);
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+
+app.UseSwagger();
+app.UseSwaggerUI();
+
 
 app.UseHttpsRedirection();
 
